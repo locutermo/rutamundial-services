1f451dceec862f00fbbeb668d0865431d51b2efd
Jose Mateo <jfmateoc@indracompany.com>
2025-02-11 00:09:10 -0500

    feat: add rate limiting to tours routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { HotelsModule } from './hotels/hotels.module';
 import { ToursModule } from './tours/tours.module';
 import { OperatorsModule } from './operators/operators.module';
 import { OffersModule } from './offers/offers.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 dotenv.config();
 
@@ -24,7 +25,24 @@ dotenv.config();
     ToursModule,
     OperatorsModule,
     OffersModule,
-
+    ThrottlerModule.forRoot([
+      {
+        //3 llamadas por segundo
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/tours/tours.controller.ts
+++ src/tours/tours.controller.ts
@@ -11,16 +11,23 @@ import {
 import { ToursService } from './tours.service';
 import { CreateTourDto } from './dto/create-tour.dto';
 import { UpdateTourDto } from './dto/update-tour.dto';
+import { SkipThrottle, Throttle } from '@nestjs/throttler';
 
+//Omite el rate limiting a todas las rutas del controllador
+@SkipThrottle()
 @Controller('tours')
 export class ToursController {
   constructor(private readonly toursService: ToursService) {}
-
+  
+  //Aplica el rate limiting a la ruta create con un limite de 5 llamadas por 30 segundos
+  @Throttle({ default: { limit: 5, ttl: 30000 } })
   @Post()
   create(@Body() createTourDto: CreateTourDto) {
     return this.toursService.create(createTourDto);
   }
 
+  //Aplica el rate limiting a la ruta findAll
+  @SkipThrottle({ default: false })
   @Get()
   async findAll(@Query('withOffers') withOffers: string) {
     const loadOffers = withOffers === 'true';
